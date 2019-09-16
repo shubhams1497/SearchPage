9fa69a6f0465e283e36480a62fa296ccc27d33fd
Shubham Swarnkar <shubham.swarnkar@ET-C02Y95GMJG5H.local>
2019-09-16 22:28:09 +0530

    Updates

---
+++ precache-manifest.f81752f70030838c5aa42a93a4626c45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "976debb3b6d163bf66bb2266dd52cbe8",
+    "url": "/SearchPage/index.html"
+  },
+  {
+    "revision": "096d8fccd6b247cd19a7",
+    "url": "/SearchPage/static/css/main.70249f4f.chunk.css"
+  },
+  {
+    "revision": "be140e10f583f25ea10d",
+    "url": "/SearchPage/static/js/2.efc0bf24.chunk.js"
+  },
+  {
+    "revision": "096d8fccd6b247cd19a7",
+    "url": "/SearchPage/static/js/main.861812c1.chunk.js"
+  },
+  {
+    "revision": "da27bfdef282da55da7b",
+    "url": "/SearchPage/static/js/runtime~main.9a4c943c.js"
+  },
+  {
+    "revision": "93064ed3cf74fd2321e5f86ee910cba7",
+    "url": "/SearchPage/static/media/star.93064ed3.svg"
+  }
+]);
