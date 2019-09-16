d3e4e62e66829e3719d4d5499892d987a80d5af7
Shubham Swarnkar <shubham.swarnkar@ET-C02Y95GMJG5H.local>
2019-09-17 01:26:06 +0530

    cart css improved

--- src/App.js
+++ src/App.js
@@ -5,12 +5,27 @@ import ProductFilters from './ProductFilters'
 import CartDisplay from './CartDisplay'
 
 class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      blurVisible: false
+    };
+    this.toggleBlurVisibility = this.toggleBlurVisibility.bind(this);
+  }
+
+  toggleBlurVisibility(){
+    this.setState({blurVisible: !this.state.blurVisible});
+  }
+
   render(){
     return (
       <div className="app-container">
         <ProductFilters/>
         <ProductDisplay/>
-        <CartDisplay/>
+        <CartDisplay toggleBlurVisibility={this.toggleBlurVisibility}/>
+        <div style={{visibility:(this.state.blurVisible)?"visible":"hidden"}}className="blur-background">
+        </div>
       </div>
     );
   }
--- src/CartDisplay.js
+++ src/CartDisplay.js
@@ -5,14 +5,40 @@ import {connect} from 'react-redux';
 
 
 class CartDisplay extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            cartVisible: false,
+        };
+        this.toggleCartVisibility = this.toggleCartVisibility.bind(this);
+    }
+
+    toggleCartVisibility(){
+        this.setState({cartVisible: !this.state.cartVisible});
+        this.props.toggleBlurVisibility();
+    }
+
     render(){
         const cartItems = this.props.allProducts.filter((product) => (product.counter>0));
         const cartItemsList = cartItems.map((product) => <ProductCard key={product.id} productInfo={product}/>)
+        const cartStyle = (this.state.cartVisible)?
+                        {transform: "scale(1)"}:
+                        {transform: "scale(0)"}
         return(
-            <div className="cart-container">
-                <h4>Cart Items</h4>
-                <div className="cart-products-list">
-                    {(cartItemsList.length>0)?cartItemsList:"Empty Cart!!"}
+            <div className="cart-wrapper">
+                <div onClick={()=> this.toggleCartVisibility()} className="cart-icon-wrapper">
+                    <img height="50px" src={(!this.state.cartVisible)?require('./cart.svg'):require('./cross.svg')} alt={"cart-icon"}/>
+                    <span style={{visibility:(!this.state.cartVisible)?"visible":"hidden"}}className="cart-item-number">{cartItems.length}</span>
+                </div>
+                <div style={cartStyle} className="cart-container">
+                    <h4>Cart Items</h4>
+                    <div className="cart-products-list">
+                        {(cartItemsList.length>0)?cartItemsList:"Empty Cart!!"}
+                    </div>
+                    <div className="total-amount">
+                        Total Amount;
+                    </div>
                 </div>
             </div>
         );
